379db19fdfaaddd020d9991ea101ddf68573a3e1
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-17 11:42:20 -0700

    Initial Commit

---
+++ Gestures-In-SwiftUI/Gestures_In_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestures_In_SwiftUIApp.swift
+//  Gestures-In-SwiftUI
+//
+//  Created by Mitya Kim on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Gestures_In_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
